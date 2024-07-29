b9cb290ff3706260933a35a9a083427ed1dbf3ba
Azula05 <127029512+Azula05@users.noreply.github.com>
2024-07-29 15:37:19 +0200

    Updated script 01
    Added a description of the script.
    added commands for the python module to be imported.
    Works with a white line to separated different inputs and make it more readable.
    Corrected error where mapping.fasta was not found if not existing before.
    Corrected error where bowtie fasta did not find the mapping.fasta
    Corrected typo on line 68
    Allows to loop all sequences and the dispalying the error which ones could not be mapped instead of on occurance.

--- bin/01_generate_template.py
+++ bin/01_generate_template.py
@@ -1,7 +1,22 @@
-#!/usr/bin/env python3
+"""
+This script is used to generate files from user input. The user input file is a text file that contains the following
+information:
+    1. The sequence with the mutation indicated by [].
+    2. The chromosome and the coordinates of the sequence. (optional)
+    3. A blank line to separate the sequences.
+    example: 
+    TGTTATGAACAGCAAATAAAAGAAACTAAAACGATCCTGAGACTTCCACACTGATGCAATCATTCGTCTGTTTCCCATTCTAAACTGTACCCTGTTACTT[A/C]TCCCCTTCCTATGACATGAACTTAACCATAGAAAAGAAGGGGAAAGAAAACATCAAGCGTCCCATAGACTCACCCTGAAGTTCTCAGGATCCACGTGCAG
+    chr11:5226403-5226603
 
+If only the sequence is provided, the tool will map the input sequence against a reference genome to determine the information.
+If the sequence is provided with the chromosome and coordinates, --coords in the pipeline should be set to 'true'.
+"""
 import argparse
+import os
+import re
 
+#######################################################################################################################
+# ARGUMENT HANDLING
 parser = argparse.ArgumentParser(description="give arguments to handle user input")
 parser.add_argument("-c", nargs=1, required=True, help="boolean if sequence coords are manually provided")
 parser.add_argument("-i", nargs=1, required=True, help="user input file")
@@ -13,12 +28,15 @@ args = parser.parse_args()
 coords = args.c[0] # 'true' (coordinates are manually provided) or 'false' (coordinates are not provided)
 usr_input_file = open(args.i[0]).readlines()
 
-# if coordinates are manually provided
+#######################################################################################################################
+####################################### COORDINATES ARE MANUALLY PROVIDED #############################################
+#######################################################################################################################
+
 if coords == "true":
     seq_ID_nr = 0
     
-    # read input file per 2 lines (coordinates are on every second line)
-    for line in range(0, len(usr_input_file), 2):
+    # read input file per 3 lines (Sequence on the first line, coordinates are on every second line, third line is a blank spacer )
+    for line in range(0, len(usr_input_file), 3):
         # Retrieve targetted SNP position from sequence
         seq = list(usr_input_file[line])
         # initialize counter to count nucleotide positions while looking for snp
@@ -36,59 +54,59 @@ if coords == "true":
 
         seq_ID_nr = seq_ID_nr + 1
 
+#######################################################################################################################
+##################################### COORDINATES ARE  NOT MANUALLY PROVIDED ##########################################
+#######################################################################################################################
 
-
-# if coordinates are manually provided, the same things are done. Now there is 
-# no need to read the user input file with 2 lines at once
 elif coords == 'false':
 
     bowtie_index = args.b[0]
     cpus = args.p[0]
 
-    seq_fasta = open("mapping.fasta").readline()
+    seq_fasta = open("mapping.fasta","w")
 
     # loop through input and make a input file for bowtie (fasta) so coordinates can be retrieved
     for i in range(0, len(usr_input_file)):
-
         # for bowtie2 to recognize the fasta file, > has to precede the sequence
         seq_fasta.write(">seq-" + str(i) + "\n")
         seq_fasta.write(usr_input_file[i].rstrip().split("[")[0] + usr_input_file[i].rstrip().split("[")[1].split("/")[0] + usr_input_file[i].rstrip().split("]")[1] + "\n")
-    
-    usr_input_file.close()
+    seq_fasta.close()
 
-    # use bowtie to map the sequences and get coordinates
-    os.system("bowtie2 -p " + cpus + " --no-hd --no-sq -x " + bowtie_index + " -f " + fasta + " -S mapping.sam")
+    #######################################################################################################################
+    ########################### use bowtie to map the sequences and get coordinates #######################################
+    #######################################################################################################################
+    # run bowtie2
+    os.system("bowtie2 -p " + cpus + " --no-hd --no-sq -x " + bowtie_index + " -f " + "mapping.fasta" + " -S mapping.sam")
 
     # get bowtie results
     sam = open('mapping.sam')
     sam_dict = {}
+    fails = []
 
     for line in sam:
         result = line.rstrip().split("\t")
-        seq_ID = results[0]
+        seq_ID = result[0]
         chrom = result[2]
         start = int(result[3])
         length = result[5]
 
+
         # when there is no mapping result
         if length == "*":
-            quit("Unable to map the input sequence, please provide sequence coordinates manually")
+            fails.append(seq_ID)
 
         length = int(re.split("(\d+)", length)[1])  # split between numbers and letters
         end = start + length
-
         sam_dict[seq_ID] = chrom + ":" + str(start) + '-' + str(end)
-
-
 
     # loop through input file again to make a file per input sequence
     usr_input_file = open(args.i[0]).readlines()
-
+    n= 0
     for i in range(0, len(usr_input_file)):
-
         seq_ID = "seq-" + str(i) 
-
         seq = list(usr_input_file[i])
+
+        # position of the snp
         c = 0
         for i in seq:
             if i == "[":
@@ -96,5 +114,9 @@ elif coords == 'false':
             c += 1
 
         seq_file = open(seq_ID + ".txt", "w")
-        seq_file.write(usr_input_file[i].rstrip() + '\t' + sam_dict[seq_ID] + '\t' + snp_pos + '\n')
+        print(usr_input_file[n])
+        seq_file.write(usr_input_file[n].rstrip() + '\t' + sam_dict[seq_ID] + '\t' + str(snp_pos) + '\n')
         seq_file.close()
+        n += 1
+    if fails != []:
+        quit("The following sequences could not be mapped: " + str(fails))
