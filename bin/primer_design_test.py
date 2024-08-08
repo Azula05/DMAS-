5fb3f81c401fa09c23e485c79b57836452fd24ee
Azula05 <127029512+Azula05@users.noreply.github.com>
2024-08-08 11:34:30 +0200

    Update primer design
    Tm calculation corrected.
    Dropped redundant part.
    Calculations now done based on the generated primers instead of starting point.

--- bin/primer_design_test.py
+++ bin/primer_design_test.py
@@ -60,7 +60,7 @@ templates = templ_generation_exchange(before, after, wt, m, seq_ID)
 
 ####################################################################################################################################################################
 
-#Zero_MM_exchange(templates, seq_ID, Single_MM_Tm):
+# Match primers needed to start from later
 """
 Shorten the templates from 35 to minimum 16 to be as close to the
 zero MM tm as possible (match). The remaining sequence is the perfect match primer.
@@ -70,14 +70,20 @@ In the library: ID, primer, Tm_0MM, length
 def match_primers(myseq, match_TM, dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc):
     myseq = Seq(myseq[-36:])
     match_TM = mt.Tm_NN(myseq, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-    while not match_TM <= ( float(Zero_MM_Tm)+0.5 ):
+    absolute_difference = 2000000
+    while not match_TM <= (float(Zero_MM_Tm)-2):
+        previous_difference = absolute_difference
         # stop if the primer gets to short
         if len(myseq) < 16:
             break
         else:
             myseq = Seq(myseq[1:])
         match_TM = mt.Tm_NN(myseq, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-    return str(myseq), '%0.3f' % match_TM, len(myseq)
+        absolute_difference = abs(match_TM - float(Zero_MM_Tm))
+        if absolute_difference < previous_difference:
+            best_seq = myseq
+            best_tm = match_TM
+    return str(best_seq),'%0.3f' % best_tm, len(best_seq)
 
 # initiate the dictionary
 primers = {}
@@ -104,52 +110,6 @@ myseq = templates[seq_ID + "_REV_MUT"]
 seq, Tm, length = match_primers(myseq, Zero_MM_Tm, dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc)
 GC_content = gc_fraction(seq) * 100
 primers[seq_ID + "_0MM_R_MUT"] = str(seq), float(Tm), float('%0.3f' % GC_content), int(length)
-
-####################################################################################################################################################################
-
-# Single_MM_exchange(templates, seq_ID, Single_MM_Tm):
-"""
-Shorten the templates from 35 to minimum 16 to be as close to the
-single MM tm as possible. The remaining sequence is the single MM primer.
-In the library: ID, primer, Tm_0MM, tm_1MM, length
-"""
-def Single_MM_primers(myseq, complement, Single_MM_Tm , dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc):
-    myseq = Seq(myseq[-36:])
-    complement = Seq(complement[-36:])
-    mismatch_TM = mt.Tm_NN(myseq, c_seq= complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-    while not mismatch_TM <= ( float(Single_MM_Tm)+0.5 ):
-        # stop if the primer gets to short
-        if len(myseq) < 16:
-            break
-        else:
-            myseq = Seq(myseq[1:])
-            complement = Seq(complement[1:])
-        mismatch_TM = mt.Tm_NN(myseq, c_seq=complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-    return str(myseq), '%0.3f' % mismatch_TM, len(myseq)
-# WT primer on the MUT template => 1MM
-primer_templates = {}
-# WT on MUT : 1MM
-## FORWARD
-forward = templates[seq_ID + "_FWD_WT"]
-complement = Seq(templates[seq_ID + "_FWD_MUT"]).complement()
-seq, Tm_1MM, length = Single_MM_primers(forward, complement, Single_MM_Tm, dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc)
-primer_templates[seq_ID + "_0" + str(Seq(m).complement()) + "_F_WT"] = seq, float(Tm_1MM), int(length)
-## REVERSE
-reverse = templates[seq_ID + "_REV_WT"]
-complement = Seq(templates[seq_ID + "_REV_MUT"]).complement()
-seq, Tm_1MM, length = Single_MM_primers(reverse, complement, Single_MM_Tm, dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc)
-primer_templates[seq_ID + "_0" + str(Seq(m).complement()) + "_R_WT"] = seq, float(Tm_1MM), int(length)
-# MUT on WT : 1MM
-## FORWARD
-forward = templates[seq_ID + "_FWD_MUT"]
-complement = Seq(templates[seq_ID + "_FWD_WT"]).complement()
-seq, Tm_1MM, length = Single_MM_primers(forward, complement, Single_MM_Tm, dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc)
-primer_templates[seq_ID + "_0" + str(Seq(wt).complement()) + "_F_MUT"] = seq, float(Tm_1MM), int(length)
-## REVERSE
-reverse = templates[seq_ID + "_REV_MUT"]
-complement = Seq(templates[seq_ID + "_REV_WT"]).complement()
-seq, Tm_1MM, length = Single_MM_primers(reverse, complement, Single_MM_Tm, dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc)
-primer_templates[seq_ID + "_0" + str(Seq(wt).complement()) + "_R_MUT"] = seq, float(Tm_1MM), int(length)
 
 ####################################################################################################################################################################
 
@@ -197,63 +157,112 @@ def exchange (seq, position_mismatch):
             seq[-position] = "G"
     return exchanged
 
+def Single_MM_Tm_match(myseq, complement, Single_MM_Tm , dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc):
+    mismatch_TM = mt.Tm_NN(myseq, c_seq= complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    absolute_difference = 2000000
+    while not mismatch_TM <= ( float(Single_MM_Tm)-2):
+        previous_difference = absolute_difference
+        # stop if the primer gets to short
+        if len(myseq) < 16:
+            break
+        else:
+            myseq = Seq(myseq[1:])
+            complement = Seq(complement[1:])
+        mismatch_TM = mt.Tm_NN(myseq, c_seq=complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+        absolute_difference = abs(mismatch_TM - float(Single_MM_Tm))
+        if absolute_difference < previous_difference:
+            best_seq = myseq
+            best_complement = complement
+            best_tm = mismatch_TM
+    return str(best_seq), str(best_complement),'%0.3f' % best_tm, len(best_seq)
+
 # WT primer on the MUT 
 ## FORWARD
-forward = primer_templates[seq_ID + "_0" + str(Seq(m).complement()) + "_F_WT"][0]
-# 1MM
-complement = primer_templates[seq_ID + "_0" + str(Seq(wt).complement()) + "_F_MUT"][0]
-complement = str(Seq(complement).complement())
-match_Tm = '%0.3f' % mt.Tm_NN(forward, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-Single_MM_Tm = '%0.3f' % mt.Tm_NN(forward, c_seq=complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-# 2MM
+forward = primers[seq_ID + "_0MM_F_WT"][0]
+# change out the 2,3,4 positions
 exchanged = exchange (forward, position_mismatch)
+# calculate the Tm for the primers
 for mutation ,oligo in exchanged.items():
-    double_MM_TM = '%0.3f' %  mt.Tm_NN(oligo, c_seq=complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    # 1MM 
+    complement_1MM = str(Seq(oligo).complement()[:-1]) + str(Seq(m).complement())
+        # change the length of the oligo to match the target Tm as close as possible
+    oligo, complement_1MM ,Single_MM_Tm, length = Single_MM_Tm_match(oligo, complement_1MM, Single_MM_Tm, dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc)
+    # 2MM
+    complement_2MM = Seq(str(templates[seq_ID + "_FWD_MUT"])[-len(oligo):]).complement()
+    # 0MM
+    match_Tm = '%0.3f' % mt.Tm_NN(oligo, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    double_MM_TM = '%0.3f' %  mt.Tm_NN(oligo, c_seq=complement_2MM, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    # GC content
     GC_content = '%0.2f' % (gc_fraction(oligo) * 100)
+    # mismatch delta
     mismatch_delta = '%0.3f' % abs(float(Single_MM_Tm) - float(double_MM_TM))
+    # add to the dictionary with the primers
     primers[seq_ID + "_" + mutation + "_F_WT"] = oligo, float(match_Tm), float(Single_MM_Tm), float(double_MM_TM), float(mismatch_delta), float(GC_content), len(oligo)
-## REVERSE
-reverse = primer_templates[seq_ID + "_0" + str(Seq(m).complement()) + "_R_WT"][0]
-# 1MM
-complement = primer_templates[seq_ID + "_0" + str(Seq(wt).complement()) + "_R_MUT"][0]
-complement = str(Seq(complement).complement())
-match_Tm = '%0.3f' % mt.Tm_NN(reverse, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-Single_MM_Tm = '%0.3f' % mt.Tm_NN(reverse, c_seq=complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)# 2MM
-exchanged = exchange (reverse, position_mismatch)
-for mutation ,oligo in exchanged.items():
-    double_MM_TM = '%0.3f' %  mt.Tm_NN(oligo, c_seq=complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-    GC_content = '%0.2f' % (gc_fraction(oligo) * 100)
-    mismatch_delta = '%0.3f' % abs(float(Single_MM_Tm) - float(double_MM_TM))
-    primers[seq_ID + "_" + mutation + "_R_WT"] = oligo, float(match_Tm), float(Single_MM_Tm), float(double_MM_TM), float(mismatch_delta), float(GC_content), len(oligo)
 
+## REVERSE
+reverse = primers[seq_ID + "_0MM_R_WT"][0]
+# change out the 2,3,4 positions
+exchanged = exchange (reverse, position_mismatch)
+# calculate the Tm for the primers
+for mutation ,oligo in exchanged.items():
+    # 1MM 
+    complement_1MM = str(Seq(oligo).complement()[:-1]) + str(Seq(m).complement())
+        # change the length of the oligo to match the target Tm as close as possible
+    oligo, complement_1MM ,Single_MM_Tm, length = Single_MM_Tm_match(oligo, complement_1MM, Single_MM_Tm, dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc)
+    # 2MM
+    complement_2MM = Seq(str(templates[seq_ID + "_REV_MUT"])[-len(oligo):]).complement()
+    # 0MM
+    match_Tm = '%0.3f' % mt.Tm_NN(oligo, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    double_MM_TM = '%0.3f' %  mt.Tm_NN(oligo, c_seq=complement_2MM, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    # GC content
+    GC_content = '%0.2f' % (gc_fraction(oligo) * 100)
+    # mismatch delta
+    mismatch_delta = '%0.3f' % abs(float(Single_MM_Tm) - float(double_MM_TM))
+    # add to the dictionary with the primers
+    primers[seq_ID + "_" + mutation + "_R_WT"] = oligo, float(match_Tm), float(Single_MM_Tm), float(double_MM_TM), float(mismatch_delta), float(GC_content), len(oligo)
 
 # MUT primer on the WT
 # ## FORWARD
-forward = primer_templates[seq_ID + "_0" + str(Seq(wt).complement()) + "_F_MUT"][0]
-# 1MM
-complement = primer_templates[seq_ID + "_0" + str(Seq(m).complement()) + "_F_WT"][0]
-complement = str(Seq(complement).complement())
-match_Tm = '%0.3f' % mt.Tm_NN(forward, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-Single_MM_Tm = '%0.3f' % mt.Tm_NN(forward, c_seq=complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-# 2MM
+forward = primers[seq_ID + "_0MM_F_MUT"][0]
+# change out the 2,3,4 positions
 exchanged = exchange (forward, position_mismatch)
+# calculate the Tm for the primers
 for mutation ,oligo in exchanged.items():
-    double_MM_TM = '%0.3f' %  mt.Tm_NN(oligo, c_seq=complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    # 1MM 
+    complement_1MM = str(Seq(oligo).complement()[:-1]) + str(Seq(m).complement())
+        # change the length of the oligo to match the target Tm as close as possible
+    oligo, complement_1MM ,Single_MM_Tm, length = Single_MM_Tm_match(oligo, complement_1MM, Single_MM_Tm, dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc)
+    # 2MM
+    complement_2MM = Seq(str(templates[seq_ID + "_FWD_WT"])[-len(oligo):]).complement()
+    # 0MM
+    match_Tm = '%0.3f' % mt.Tm_NN(oligo, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    double_MM_TM = '%0.3f' %  mt.Tm_NN(oligo, c_seq=complement_2MM, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    # GC content
     GC_content = '%0.2f' % (gc_fraction(oligo) * 100)
+    # mismatch delta
     mismatch_delta = '%0.3f' % abs(float(Single_MM_Tm) - float(double_MM_TM))
+    # add to the dictionary with the primers
     primers[seq_ID + "_" + mutation + "_F_MUT"] = oligo, float(match_Tm), float(Single_MM_Tm), float(double_MM_TM), float(mismatch_delta), float(GC_content), len(oligo)
 
 ## REVERSE
-reverse = primer_templates[seq_ID + "_0" + str(Seq(wt).complement()) + "_R_MUT"][0]
-# 1MM
-complement = primer_templates[seq_ID + "_0" + str(Seq(m).complement()) + "_R_WT"][0]
-complement = str(Seq(complement).complement())
-match_Tm = '%0.3f' % mt.Tm_NN(reverse, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-Single_MM_Tm = '%0.3f' % mt.Tm_NN(reverse, c_seq=complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
-# 2MM
+reverse = primers[seq_ID + "_0MM_R_MUT"][0]
+# change out the 2,3,4 positions
 exchanged = exchange (reverse, position_mismatch)
+# calculate the Tm for the primers
 for mutation ,oligo in exchanged.items():
-    double_MM_TM = '%0.3f' %  mt.Tm_NN(oligo, c_seq=complement, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    # 1MM 
+    complement_1MM = str(Seq(oligo).complement()[:-1]) + str(Seq(m).complement())
+        # change the length of the oligo to match the target Tm as close as possible
+    oligo, complement_1MM ,Single_MM_Tm, length = Single_MM_Tm_match(oligo, complement_1MM, Single_MM_Tm, dnac, Na_conc, K_conc,Tris_conc,Mg_conc,dNTPs_conc)
+    # 2MM
+    complement_2MM = Seq(str(templates[seq_ID + "_REV_WT"])[-len(oligo):]).complement()
+    # 0MM
+    match_Tm = '%0.3f' % mt.Tm_NN(oligo, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    double_MM_TM = '%0.3f' %  mt.Tm_NN(oligo, c_seq=complement_2MM, nn_table=mt.DNA_NN4, saltcorr=7, dnac1=dnac, dnac2=dnac, Na=Na_conc, K=K_conc, Tris=Tris_conc, Mg=Mg_conc, dNTPs=dNTPs_conc)
+    # GC content
     GC_content = '%0.2f' % (gc_fraction(oligo) * 100)
+    # mismatch delta
     mismatch_delta = '%0.3f' % abs(float(Single_MM_Tm) - float(double_MM_TM))
+    # add to the dictionary with the primers
     primers[seq_ID + "_" + mutation + "_R_MUT"] = oligo, float(match_Tm), float(Single_MM_Tm), float(double_MM_TM), float(mismatch_delta), float(GC_content), len(oligo)
+print(primers)
