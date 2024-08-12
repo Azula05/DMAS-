89931f6a90667597a93665dc76a5e571e093e728
Azula05 <127029512+Azula05@users.noreply.github.com>
2024-08-12 16:36:31 +0200

    update primer validation
    started the code to search for SNPs and sec structure in the primers. Not finished yet

--- bin/07_primer_validation.py
+++ bin/07_primer_validation.py
@@ -21,6 +21,7 @@ parser.add_argument("-s", nargs=1, required=True, help="file with the SNP inform
 parser.add_argument("-u", nargs=1, required=True, help="file with the secondary structure information")
 parser.add_argument("-o", nargs=1, required=True, help="tsv file with the primer information")
 parser.add_argument("-p", nargs=1, required=True, help="file with the primer3 settings")
+parser.add_argument("-q", nargs=1, required=True, help="upfront checks: yes, snp, str, no")
 args = parser.parse_args()
 
 # Get the file names from the arguments
@@ -34,7 +35,10 @@ primer3_settings= args.p[0]
 # get the ID from the file
 ID = file_template.split("_")[0].replace(".txt", "")
 ID = ID.split("/")[-1]
-
+# upfront check
+checks = args.q[0]
+SNP_avoid_range = []
+sec_str_avoid_range = []
 ####################################################################################################
 ##############################   Add common primers to the table   #################################
 ####################################################################################################
@@ -120,9 +124,15 @@ Reverse.close()
 with open(file_output, 'r') as table:
     lines = table.readlines()
 template_file = open(file_template, 'r')
-# get the template sequence
+# Info from the input file
 for line in template_file:
     if line != "":
+        seq, pos, position_of_interest = line.split('\t')
+        chrom, start, end = pos.replace(':', '-').split('-')
+        start = int(start)
+        end = int(end)
+        position_of_interest = int(position_of_interest)
+        seq_length = len(seq)-4
         left = line.split("[")[0]
         wt = line.split("[")[1].split("]")[0].split("/")[0]
         m = line.split("]")[0].split("/")[1]
@@ -231,18 +241,119 @@ with open(file_output,'w') as output:
                 line = line.rstrip() + "\t" + common_FWD + "\t" + common_TM + "\t" + common_GC + "\t" + str(len(common_FWD))  + "\t" + "primer3 failed to validate primers" + "\t" + "primer3 failed to validate primers"+ "\n"
                 output.write(line)
 output.close()
-
+table.close()
 
 
 ####################################################################################################
 #######################################   Check for SNPs   #########################################
 ####################################################################################################
-
+if checks == "yes" or checks =="YES" or checks == "snp" or checks == "SNP":
+    # open the SNP file
+    SNPs = open(args.s[0])
+    # loop all the SNPs that where found
+    for line in SNPs:
+        # if the line is not empty
+        if line != "":
+            line = line.split("\t")
+            position = int(line[1])
+            position = position - start + 1 # 0-based
+            # if the position is recognized as a SNP, ignore
+            if position == position_of_interest:
+                continue
+            else:
+                SNP_avoid_range.append(position)
+    SNPs.close()
 ####################################################################################################
 #################################   Check for secondary structures   ###############################
 ####################################################################################################
+if checks == "yes" or checks =="YES" or checks == "str" or checks == "STR":
+    # open the secondary structure file
+    sec_str = open(args.u[0])
+    # loop all the secondary structures that where found
+    for line in sec_str:
+        # if the line is not empty
+        if line != "":
+            line = line.split("\t")
+            structure = line[1]
+        # make a list with the positions to avoid
+        i = 0
+        for char in list(structure):
+            if char == "(":
+                sec_str_avoid_range.append(i)
+            elif char == ")":
+                sec_str_avoid_range.append(i)
+            i += 1
+    sec_str.close()
 
 ####################################################################################################
-######################################   Add amplicon   ############################################
+#################################   Add amplicon and postions found  ###############################
 ####################################################################################################
+
+# function to find the amplicon and positions
+def get_amplicon(template, forward_primer, reverse_primer):
+    # Find the start position of the forward primer
+    forward_start = template.find(forward_primer)
+    if forward_start == -1:
+        raise ValueError("Forward primer not found in template sequence")
+    
+    # Find the start position of the reverse primer (on the reverse complement strand)
+    reverse_complement = str(Seq(reverse_primer).reverse_complement())
+    reverse_start = template.find(reverse_complement)
+    reverse_end = reverse_start + len(reverse_complement)
+    if reverse_start == -1:
+        raise ValueError("Reverse primer not found in template sequence")
+    
+    # Extract the amplicon
+    amplicon = template[forward_start:reverse_start + len(reverse_complement)]
+    return amplicon, forward_start, reverse_end
+
+# function to check the primers
+def check_primer_positions(forward_start, reverse_end, SNP_avoid_range, sec_str_avoid_range):
+    # check the forward primer
+    SNPs_FWD = []
+    sec_FWD = []
+    for position in SNP_avoid_range:
+        if position >= forward_start and position <= (forward_start + len(forward)):
+            SNPs_FWD.append(position)
+    for position in sec_str_avoid_range:
+        if position >= forward_start and position <= (forward_start + len(forward)):
+            sec_FWD.append(position)
+    if SNPs_FWD == []:
+        SNPs_FWD = "No SNPs found"
+    if sec_FWD == []:
+        sec_FWD = "No secondary structures found"
+    # check the reverse primer
+    positions_REV = range(reverse_end+1 - len(reverse), reverse_end)
+    SNPs_REV = []
+    sec_REV = []
+    for position in SNP_avoid_range:
+        if position >= reverse_end+1 - len(reverse) and position <= reverse_end:
+            SNPs_REV.append(position)
+    for position in sec_str_avoid_range:
+        if position >= reverse_end+1 - len(reverse) and position <= reverse_end:
+            sec_REV.append(position)
+    if SNPs_REV == []:
+        SNPs_REV = "No SNPs found"
+    if sec_REV == []:
+        sec_REV = "No secondary structures found"
+    return SNPs_FWD, sec_FWD, SNPs_REV, sec_REV
+
+# open the table file
+with open(file_output, 'r') as table:
+    lines = table.readlines()
+with open(file_output, 'w') as output:
+    for line in lines:
+        line = line.rstrip().split("\t")
+        if "F_WT" in line[0]:
+            template = left + wt + right
+            forward = left + wt
+            forward = forward[-len(line[1]):]
+            reverse = line[8]
+            amplicon, forward_start, reverse_end = get_amplicon(template, forward, reverse) #0-based
+            SNPs_FWD, sec_FWD, SNPs_REV, sec_REV = check_primer_positions(forward_start, reverse_end, SNP_avoid_range, sec_str_avoid_range)
+            output.write("\t".join(line) + "\t" + str(SNPs_FWD) + "\t" + str(sec_FWD) + "\t" + str(SNPs_REV)+ "\t" + str(sec_REV) + "\t" + str(amplicon) + "\n")
+        
+
+
 table.close()
+output.close()
