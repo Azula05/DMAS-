d81dfeac05b6455b117bb4c652c2fb6cff0f945f
Azula05 <127029512+Azula05@users.noreply.github.com>
2024-08-13 16:03:19 +0200

    Update sec_structure_template
    No longer selects the worst secondary structure but keeps both the wild type and mutant template files.

--- bin/03_get_sec_str_temp_ViennaRNA.py
+++ bin/03_get_sec_str_temp_ViennaRNA.py
@@ -56,34 +56,30 @@ MUT_structure = arguments[3].split(" ")[0]
 MUT_delta_g = arguments[2].split(" ",1)[1]
 
 ############################################################################################
-#################################    comparison   ##########################################
+#################################    Output files   ########################################
 ############################################################################################
-# which sequence has the worst secondary structures? wt or mut?
-# write structure and deltag of worst sequence to output file
-## They are equal
-if WT_structure == MUT_structure:
-    output = open("sec-str_" + seq_ID + '_var-wtmt' + ".txt", "a")
-    output.write(seq_ID + '_var-wtmt' + '\t' + MUT_structure + "\t" + str(MUT_delta_g) + "\t")
-## WT has the worst secondary structures
-elif (WT_structure.count("(") + WT_structure.count(")")) > (MUT_structure.count("(") + MUT_structure.count(")")):
-    output = open("sec-str_" + seq_ID + '_var-wt'+ ".txt", "a")
-    output.write( seq_ID + '_var-wt'+ '\t' + WT_structure + "\t" + str(WT_delta_g) + "\t")
-## MUT has the worst secondary structures
-elif (WT_structure.count("(") + WT_structure.count(")")) < (MUT_structure.count("(") + MUT_structure.count(")")):
-    output = open("sec-str_" + seq_ID + '_var-mt'+ ".txt", "a")
-    output.write(seq_ID + '_var-mt'+ '\t' + MUT_structure + "\t" + str(MUT_delta_g) + "\t")
-    structure = MUT_structure
 
+# Wild type
+output_wt = open("sec-str_" + seq_ID + '_wt' + ".txt", "a")
+output_wt.write(seq_ID + '_wt' + '\t' + WT_structure + "\t" + str(WT_delta_g) + "\t")
 # Which positions have secondary structures?
 str_not_ok = []
 # if seq has secondary structures ("(" or ")"), retrieve the positions and add it to a list
-for index, j in enumerate(structure):
+for index, j in enumerate(WT_structure):
     if j == "(" or j == ")":
         str_not_ok.append(index) # 0-based
+output_wt.write(str(str_not_ok) + "\n")
+output_wt.close()
 
-# write list with positions of secondary structures to output file
-# generate the output file
+## Mutant
+output_mut = open("sec-str_" + seq_ID + '_mut'+ ".txt", "a")
+output_mut.write( seq_ID + '_var-wt'+ '\t' + MUT_structure + "\t" + str(MUT_delta_g) + "\t")
+# Which positions have secondary structures?
+str_not_ok = []
+# if seq has secondary structures ("(" or ")"), retrieve the positions and add it to a list
+for index, j in enumerate(MUT_structure):
+    if j == "(" or j == ")":
+        str_not_ok.append(index) # 0-based
+output_mut.write(str(str_not_ok) + "\n")
+output_mut.close()
 
-output.write(str(str_not_ok) + "\n")
-
-output.close()
